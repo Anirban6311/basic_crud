84b3d05f6982e3c797b6ad2273c1a29f55f38f69
Anirban Sinha Roy <asinharoy33@gmail.com>
2024-03-14 23:38:56 +0530

    completed

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,7 @@
+
+
+import 'package:basic_crud_flutter/service/database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +15,76 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController _nameController= TextEditingController();
+  TextEditingController _ageController=TextEditingController();
+  TextEditingController _locationController=TextEditingController();
 
+  Stream? EmployeeStream;
+
+  getontheload() async{
+    EmployeeStream =await DatabaseMethods().getEmployeeDetails();
+    setState(() {
+
+    });
+  }
+
+  @override
+  void initState() {
+    getontheload();
+    super.initState();
+  }
+  Widget allEmployeeDetails(){
+    return StreamBuilder(stream: EmployeeStream, builder: (context,AsyncSnapshot snapshot) {
+       return snapshot.hasData? ListView.builder(itemCount: snapshot.data.docs.length, itemBuilder: (context,index){
+         DocumentSnapshot ds=snapshot.data.docs[index];
+         return Material(
+           child: Container(
+             margin: EdgeInsets.only(left: 20,right: 20,top: 30,bottom: 30),
+             child: Column(
+               children: [
+                 Material(child: Container(
+                     padding: EdgeInsets.symmetric(horizontal: 20),
+                     width: double.infinity,
+                     color: Colors.amberAccent,
+                     child: Column(
+                       mainAxisSize: MainAxisSize.min,
+                       crossAxisAlignment: CrossAxisAlignment.start,
+                       children: [
+                         Row(
+                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                           children: [
+                             Text("Name : "+ds["Name"],style: TextStyle(color: Colors.blueAccent,fontSize: 20),),
+                             Spacer(),
+                             GestureDetector(
+                                 onTap: (){
+                                   //showing the details in the boxe(textfields) for editng using ID
+                                   _nameController.text=ds["Name"];
+                                   _ageController.text=ds["Age"];
+                                   _locationController.text=ds["Location"];
+                                   editEmployeeDetails(ds["Id"]);  ///accessing the function through Id
+                                 },
+                                 child: Icon(Icons.edit,color: Colors.redAccent,)),
+                                 GestureDetector(
+                                     onTap: ()async{
+                                       await DatabaseMethods().deleteEmployeeDetails(ds["Id"]);
+                                     },
+                                     child: Icon(Icons.delete,color: Colors.red,)),
+                           ],
+                         ),
+
+                         Text("Age : "+ds["Age"],style: TextStyle(color: Colors.blueAccent,fontSize: 20),),
+                         Text("Location : "+ds["Location"],style: TextStyle(color: Colors.blueAccent,fontSize: 20),),
+                       ],
+                     ))),
+               ],
+             ),
+           ),
+         );
+
+
+       }):Container();
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,9 +93,150 @@ class _HomeScreenState extends State<HomeScreen> {
       }),
       appBar: AppBar(
         title: Center(child: Text("Home")),
-      ),
 
+      ),
+      body: Container(
+        margin: EdgeInsets.only(left: 20,right: 20,top: 30,bottom: 30),
+        child: Column(
+          children: [
+            Expanded(child: allEmployeeDetails())
+          ],
+        ),
+      )
 
     );
   }
+  Future editEmployeeDetails(String id)=> showDialog(context: context, builder: (context)=>AlertDialog(
+    content:SingleChildScrollView(
+  child: Column(
+  mainAxisSize: MainAxisSize.min,
+    children: [
+      Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          GestureDetector(
+            onTap: () {
+              Navigator.pop(context);  // dialog box will get removed
+            },
+            child: Icon(Icons.cancel),
+          ),
+          SizedBox(width: 10), // Adjust spacing between icon and text
+          Text(
+            "Edit details",
+            style: TextStyle(fontSize: 30, color: Colors.amberAccent),
+          ),
+        ],
+      ),
+      SizedBox(height: 30),
+      Text("Name"),
+      SizedBox(height: 20),
+      Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+          border: Border.all(),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: TextField(
+          controller: _nameController,
+          decoration: InputDecoration(border: InputBorder.none),
+        ),
+      ),
+      SizedBox(height: 20),
+      Text("Age"),
+      SizedBox(height: 20),
+      Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+          border: Border.all(),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: TextField(
+          controller: _ageController,
+          decoration: InputDecoration(border: InputBorder.none),
+        ),
+      ),
+      SizedBox(height: 20),
+      Text("Location"),
+      SizedBox(height: 20),
+      Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        decoration: BoxDecoration(
+          border: Border.all(),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: TextField(
+          controller: _locationController,
+          decoration: InputDecoration(border: InputBorder.none),
+        ),
+      ),
+      SizedBox(height: 10),
+      ElevatedButton(onPressed: () async{
+            Map<String,dynamic> updateInfo={
+              "Name": _nameController.text,
+              "Age" : _ageController.text,
+              "Id": id,
+              "Location" : _locationController.text,
+
+            };
+            //after clicking update the dialog box will be popped
+            await DatabaseMethods().updateEmployeeDetails(id, updateInfo).then((value){
+              Navigator.pop(context);
+            });
+      }, child: Text("Update"))
+
+      // child: Row(
+        //   children: [
+
+            // SizedBox(height: 30,),
+            // Text("Name"),
+            // SizedBox(height: 20,),
+            // Container(
+            //   padding: EdgeInsets.symmetric(horizontal: 20),
+            //   decoration: BoxDecoration(
+            //     border: Border.all(),
+            //     borderRadius: BorderRadius.circular(20),
+            //   ),
+            //
+            //   child: TextField(
+            //     controller: _nameController,
+            //     decoration: InputDecoration(border: InputBorder.none),
+            //   ),
+            // ),
+            // SizedBox(height: 20,),
+            // Text("Age"),
+            // SizedBox(height: 20,),
+            // Container(
+            //   padding: EdgeInsets.symmetric(horizontal: 20),
+            //   decoration: BoxDecoration(
+            //     border: Border.all(),
+            //     borderRadius: BorderRadius.circular(20),
+            //   ),
+            //
+            //   child: TextField(
+            //     controller: _ageController,
+            //     decoration: InputDecoration(border: InputBorder.none),
+            //   ),
+            // ),
+            // SizedBox(height: 20,),
+            // Text("Location"),
+            // SizedBox(height: 20,),
+            // Container(
+            //   padding: EdgeInsets.symmetric(horizontal: 20),
+            //   decoration: BoxDecoration(
+            //     border: Border.all(),
+            //     borderRadius: BorderRadius.circular(20),
+            //   ),
+            //
+            //   child: TextField(
+            //     controller: _locationController,
+            //     decoration: InputDecoration(border: InputBorder.none),
+            //   ),
+            // ),
+            // SizedBox(height: 5,),
+
+          ],
+        ),
+      ),
+    ),
+  );
 }
--- lib/service/database.dart
+++ lib/service/database.dart
@@ -1,5 +1,8 @@
 
+
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 // for deleting ,updating our databases
 class DatabaseMethods{
   Future addEmployeeDetails(Map<String,dynamic> employeeInfoMap,String id) async{
@@ -8,4 +11,29 @@ class DatabaseMethods{
         .doc(id)
         .set(employeeInfoMap);
   }
+   ///to fetch data from the collection in the form of snapshots
+  Future<Stream<QuerySnapshot>> getEmployeeDetails() async{
+    return await FirebaseFirestore.instance.collection("Employee").snapshots(
+
+    );
+  }
+
+  //function to update the details
+  Future updateEmployeeDetails(String id,Map<String,dynamic> updateInfo) async{
+    return await FirebaseFirestore.instance
+        .collection("Employee")
+        .doc(id)
+         .update(updateInfo)
+    ;
+  }
+  // deletion
+  Future deleteEmployeeDetails(String id) async{
+    return await FirebaseFirestore.instance
+        .collection("Employee")
+        .doc(id)
+        .delete()
+
+    ;
+  }
+
 }
